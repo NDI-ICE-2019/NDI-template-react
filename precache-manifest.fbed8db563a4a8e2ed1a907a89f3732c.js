72452ac99ac4ed7c53163045e37a9270f5d32daa
GuillaumeDuc <ducoeurguillaume@gmail.com>
2019-12-06 06:59:57 +0100

    Updates

---
+++ precache-manifest.fbed8db563a4a8e2ed1a907a89f3732c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d80b45e06975e7d8a65083a84f6987",
+    "url": "/precaritorasor/index.html"
+  },
+  {
+    "revision": "7e548a0033b767d2893f",
+    "url": "/precaritorasor/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a7790888a3142540477f",
+    "url": "/precaritorasor/static/css/main.7de15cfe.chunk.css"
+  },
+  {
+    "revision": "7e548a0033b767d2893f",
+    "url": "/precaritorasor/static/js/2.a08c975d.chunk.js"
+  },
+  {
+    "revision": "a7790888a3142540477f",
+    "url": "/precaritorasor/static/js/main.f4258b78.chunk.js"
+  },
+  {
+    "revision": "5a3640ad57d1923373f8",
+    "url": "/precaritorasor/static/js/runtime~main.c78b43d0.js"
+  }
+]);
